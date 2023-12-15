75398445e427e439bd5b0f8865a88c651b331024
FUAD GETACHEW <fuadgetachew2@gmail.com>
2023-12-15 21:20:38 +0300

    Structure and Lay Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 // eslint-disable-next-line no-unused-vars
+import styles from "./style";
+
+import {Navbar,Stats,Business, CardDeal, Testimonials, Clients ,CTA ,Footer, Biling,Hero} from "./components";
+
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello, world</h1>
+   <div className="{`${styles.paddingX}`}">
+    <div className="{`${styles.boxWidth}`}">
+    <Navbar/> 
+    </div>
+   </div>
+     
+   <div className="{`${styles.paddingX}`}">
+    <div className="{`${styles.boxWidth}`}">
+    <Hero />
+
+    </div> 
+   </div>
+
+  <div className="bg-primary $`{styles.paddingX}` w-full overflow-hidden">
+   <div className="{`${styles.paddingX}`}">
+    <div className="{`${styles.boxWidth}`}">
+      <Stats/>
+      <Business/>
+      <CardDeal/>
+      <Testimonials/>
+      <Clients/>
+      <CTA/>
+      <Footer/> 
+      <Biling/>
+
+    </div>
+   </div>
+  </div>
+  
   </div>
 );
 
